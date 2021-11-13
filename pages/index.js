a932d29ba20f86e2419e8ff8b3123aa8217fa30b
Yik San Chan <evan.chanyiksan@gmail.com>
2021-11-13 23:37:53 +0800

    remove link to vercel

--- pages/index.js
+++ pages/index.js
@@ -70,11 +70,9 @@ export default function Home() {
       <footer className="flex items-center justify-center w-full h-24 border-t">
         <a
           className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="https://yiksanchan.com/"
         >
-          Built by @yiksanchan
+          Built by yiksanchan
         </a>
       </footer>
     </div>
