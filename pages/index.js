f8dee454120fb05133d1da1382d3887d81bb1fc6
Yik San Chan <evan.chanyiksan@gmail.com>
2021-11-14 00:08:14 +0800

    re-layout

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,13 @@ import Select from "react-select";
 // TODO: load static file
 const data = `Uber,eng.uber.com
 Airbnb,medium.com/airbnb-engineering
-Lyft,eng.lyft.com`;
+Lyft,eng.lyft.com
+Pinterest,medium.com/pinterest-engineering
+Netflix,netflixtechblog.com
+Twitter,blog.twitter.com/engineering
+Facebook,engineering.fb.com/`;
 
+// TODO: order by label
 const blogs = data.split("\n").map((s) => {
   const a = s.split(",");
   return { label: a[0], value: a[1] };
@@ -22,21 +27,18 @@ function getGoogleSearchQuery(term, sites) {
   );
 }
 
+// TODO: understand and cleanup class names
 export default function Home() {
   const [searchTerm, setSearchTerm] = React.useState("");
   const [selectedBlogs, setSelectedBlogs] = React.useState(blogs);
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center min-h-screen">
       <Head>
         <title>How X Do Y</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center flex-1 px-20">
-        <h1 className="text-6xl font-bold">
-          Welcome to <a className="text-blue-600">How X Do Y</a>
-        </h1>
-
+      <main className="flex items-center flex-1 w-1/4">
         <div className="w-full">
           <p>How</p>
           <Select
@@ -52,18 +54,21 @@ export default function Home() {
           <p>Do</p>
           <div className="flex flex-col">
             <input
-              className="border border-gray-300 rounded px-2 h-10"
+              className="border border-gray-300 rounded px-3 h-10"
               type="text"
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
             />
           </div>
-          <p>
-            <a className="text-blue-600" href={getGoogleSearchQuery(
+          <a
+            className="text-blue-600"
+            href={getGoogleSearchQuery(
               searchTerm,
               selectedBlogs.map((blog) => blog.value)
-            )}>Go!</a>
-          </p>
+            )}
+          >
+            Go!
+          </a>
         </div>
       </main>
 
