1c2219a5d9706ca2d547b623a4bdb61c73e0c86c
Yik San Chan <evan.chanyiksan@gmail.com>
2021-11-13 23:27:35 +0800

    generate google search query

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,28 @@ import React from "react";
 
 import Select from "react-select";
 
+// TODO: load static file
 const data = `Uber,eng.uber.com
 Airbnb,medium.com/airbnb-engineering
 Lyft,eng.lyft.com`;
 
 const blogs = data.split("\n").map((s) => {
   const a = s.split(",");
-  return {label: a[0], value: a[1]};
+  return { label: a[0], value: a[1] };
 });
 
+// TODO: it makes no sense when sites=[]
+function getGoogleSearchQuery(term, sites) {
+  return encodeURI(
+    `https://google.com/search?q=${term} (${sites
+      .map((site) => `site:${site}`)
+      .join(" OR ")})`
+  );
+}
+
 export default function Home() {
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const [selectedBlogs, setSelectedBlogs] = React.useState(blogs);
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
@@ -20,7 +32,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-50 flex-1 px-20">
+      <main className="flex flex-col items-center justify-center flex-1 px-20">
         <h1 className="text-6xl font-bold">
           Welcome to <a className="text-blue-600">How X Do Y</a>
         </h1>
@@ -30,18 +42,28 @@ export default function Home() {
           <Select
             defaultValue={blogs}
             isMulti
-            name="colors"
+            name="blogs"
             options={blogs}
-            className="basic-multi-select"
             classNamePrefix="select"
+            onChange={(selected) => {
+              setSelectedBlogs(selected);
+            }}
           />
           <p>Do</p>
           <div className="flex flex-col">
             <input
               className="border border-gray-300 rounded px-2 h-10"
               type="text"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
             />
           </div>
+          <p>
+            <a className="text-blue-600" href={getGoogleSearchQuery(
+              searchTerm,
+              selectedBlogs.map((blog) => blog.value)
+            )}>Go!</a>
+          </p>
         </div>
       </main>
 
