ac47b9fa20673cc4c2b593d714967cd1bd5e3a8c
Yik San Chan <evan.chanyiksan@gmail.com>
2021-11-13 22:15:30 +0800

    cleanup

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,15 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>How X Do Y</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
+          <a className="text-blue-600">
+            How X Do Y
           </a>
         </h1>
 
